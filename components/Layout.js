b224b3c412e181bd8a73bf3e33ff23cdee0222bd
AbdUlraHim0 <3bdulrahimelsherbiny@gmail.com>
2022-05-25 17:20:23 +0200

    Dark & Light Theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,60 @@
 import React from "react"
 import Head from "next/head"
 import NextLink from "next/link"
-import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material"
+import {
+  AppBar,
+  Container,
+  createTheme,
+  CssBaseline,
+  Link,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material"
 import useStyles from "../utils/styles"
+import { ThemeProvider } from "@mui/material"
+import { useContext } from "react"
+import { Store } from "../context/Store"
+import Cookies from "js-cookie"
 
 export default function Layout({ title, children, description }) {
+  const { state, dispatch } = useContext(Store)
+  const { darkMode } = state
+
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  })
+
   const classes = useStyles()
+
+  const darkModeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" })
+    const newDarkMode = !darkMode
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF")
+  }
 
   return (
     <div>
@@ -15,30 +64,34 @@ export default function Layout({ title, children, description }) {
         </title>
         {description && <meta name='description' content={description}></meta>}
       </Head>
-      <AppBar position='static' className={classes.navbar}>
-        <Toolbar>
-          <NextLink href='/' passHref>
-            <Link>
-              <Typography className={classes.brand}>
-                NextJs_E-Commerce
-              </Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href='/cart' passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position='static' className={classes.navbar} color='secondary'>
+          <Toolbar>
+            <NextLink href='/' passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  NextJs_E-Commerce
+                </Typography>
+              </Link>
             </NextLink>
-            <NextLink href='/login' passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>all rights reserved. Next.js_E-Commerce</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch checked={darkMode} onChange={darkModeHandler}></Switch>
+              <NextLink href='/cart' passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href='/login' passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>all rights reserved. Next.js_E-Commerce</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
